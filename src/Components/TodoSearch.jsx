0dd4b9fbc251714304c04053f9b2aef32b52d9ee
segundo rebaza <alvaro.rp1593@gmail.com>
2022-04-26 02:20:40 -0500

    add events todo search

--- src/Components/TodoSearch.jsx
+++ src/Components/TodoSearch.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import '../TodoSearch.css';
 
 const TodoSearch = () => {
+  const onSearchValueChange = (event) => {
+    console.log(event.target.value)
+  }
+
   return (
-    <input className="TodoSearch" placeholder="Search" />
+    <input onChange={onSearchValueChange} className="TodoSearch" placeholder="Search" />
+    // onChange={(event) => console.log(event.target.value)} Puede ir solo esto aqui en vez de la arrow function de arriba
   )
 }
 
