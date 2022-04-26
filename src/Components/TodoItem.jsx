63685639eae6188dcbb566d8a15b9a10163239cf
segundo rebaza <alvaro.rp1593@gmail.com>
2022-04-26 02:12:29 -0500

    add events todo items

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -3,14 +3,19 @@ import '../TodoItem.css';
 
 const TodoItem = (props) => {
   const onComplete = () => {
-    alert('Ya completaste el todo ' + props.text)
-  }
+    alert('Ya completaste el todo ' + props.text);
+  };
+
+  const onDelete = () => {
+    alert('Borraste el todo ' + props.text);
+  };
 
   return (
     <li className="TodoItem">
-      <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>√</span>
+      <span onClick={onComplete} className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>√</span>
+      {/* En onclick solo llamas a la funcion sin hacer arrow function porque la funcion ya es una arrow, si tuviera parametros la funcion alli si pondria como una arrow function */}
       <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
-      <span className="Icon Icon-delete">X</span>
+      <span onClick={onDelete} className="Icon Icon-delete">X</span>
     </li>
   )
 }
